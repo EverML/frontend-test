c661130b14c344f95f4233f22e94bbd8398b3261
Ever Leiva <everleiva21@gmail.com>
2020-10-20 21:38:51 -0300

    Updates

---
+++ precache-manifest.c638e5bbff46b07420026c7b7b2bbfc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950413628d1753a2653b1bc7bf933dec",
+    "url": "/frontend-test/index.html"
+  },
+  {
+    "revision": "9a83faf6146168c76f06",
+    "url": "/frontend-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e93079bc3ee247de07b1",
+    "url": "/frontend-test/static/js/2.e04a291c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/frontend-test/static/js/2.e04a291c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a83faf6146168c76f06",
+    "url": "/frontend-test/static/js/main.71f978f5.chunk.js"
+  },
+  {
+    "revision": "7f43fe872a7a4b4cafb8",
+    "url": "/frontend-test/static/js/runtime-main.afc04f91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontend-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
