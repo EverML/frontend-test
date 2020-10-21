f0ff99366e41dff0bcd71dffa7bc5a2180420885
Ever Leiva <everleiva21@gmail.com>
2020-10-21 09:56:57 -0300

    Corregido package.json (dependencias de material-ui)

--- frontend/src/components/Loading/index.js
+++ frontend/src/components/Loading/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import {CircularProgress} from '@material-ui/core/CircularProgress';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 const LoadingContainer = styled.div`
   display:flex;
+  padding:1rem;
   align-items:center;
   justify-content: center;
   height:100vh;
