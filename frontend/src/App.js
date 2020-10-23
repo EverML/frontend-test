0b6b03fcec5c52b9abd6144d9ba105ebc4228e89
Ever Leiva <everleiva21@gmail.com>
2020-10-22 21:42:24 -0300

    Agregado router y header mejorado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,45 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import "./App.css";
 
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 
-function App() {
-  return (
-    <div>
-        <Header />
-        <Layout />
+function About() {
+  return <h2>About</h2>;
+}
 
-    </div>
-  );
+function Users() {
+  return <h2>Users</h2>;
+}
+
+
+function App() {
+
+  return <Router>
+      <div>
+        <Switch>
+          <Route path="/about">
+            <Header />
+            <About />
+          </Route>
+          <Route path="/users">
+            <Header />
+            <Users />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Layout />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
 }
 
 export default App;
--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,31 +1,77 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import styled from '@emotion/styled';
 
-const HeaderContainer = styled.div`
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  padding: 1rem;
-  
-  text-align:left;
-  border-bottom: 1px solid black;
-  width:100%;
-`;
+import Toolbar from '@material-ui/core/Toolbar';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const InnerContainer = styled.div`
-  width:100%;
-  max-width:1120px;
-`;
-
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 function Header() {
-  return <HeaderContainer>
-            <InnerContainer>
-              <h1>Events</h1>
-            </InnerContainer>
-         </HeaderContainer>
 
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Events
+        </Typography>
+        <Button  color="inherit" component={Link} to="/">Home</Button>
+        <Button  color="inherit" component={Link} to="about">About</Button>
+        <Button  color="inherit" component={Link} to="/users">Users</Button>
+      </Toolbar>
+    </AppBar>
+  </div>
+  /*
+    <HeaderContainer>
+      <InnerContainer>
+        <h1>Events</h1>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </InnerContainer>
+    </HeaderContainer>*/
+  );
 }
 
 export default Header;
